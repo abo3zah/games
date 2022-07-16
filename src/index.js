1cd91dbb26ee5ed392daf996128ee5365fbb71af
abo3zah <31366659+abo3zah@users.noreply.github.com>
2022-07-17 01:44:38 +0300

    adding HashRouter instead of BrowseRoute

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route, HashRouter } from "react-router-dom";
 import './index.css';
 import { TicTacToe } from './routes/tic-tac-toe.js'
 import { NumbersOrganizer } from './routes/numbers-organizer.js'
@@ -9,12 +9,12 @@ import App from './app.js'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/Tic-Tac-Toe" element={<TicTacToe />} />
         <Route path="/Number-Organizer" element={<NumbersOrganizer />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
