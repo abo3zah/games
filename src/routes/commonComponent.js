86332d6e225544569a5c639d75c23b7bbcade99e
abo3zah <31366659+abo3zah@users.noreply.github.com>
2022-07-26 00:15:04 +0300

    visual enhancement

--- src/routes/commonComponent.js
+++ src/routes/commonComponent.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export function ReturnButton(){
      return(
-       <Link to="/" className="text-center self-start w-full bg-slate-100 border border-black rounded">
+       <Link to="/" className="text-center self-start w-full bg-slate-100 border border-black rounded h-10 grid items-center">
          Back to Home
        </Link>
      )
